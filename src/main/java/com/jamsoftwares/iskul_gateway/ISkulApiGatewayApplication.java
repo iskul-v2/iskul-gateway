c5fa41542ba9db6bed704ab6281a168e36b50557
Jamel Cagampang <jamcagampang@gmail.com>
2025-05-12 22:38:16 +0800

    Initial Commit

---
+++ src/main/java/com/jamsoftwares/iskul_gateway/ISkulApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.jamsoftwares.iskul_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISkulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISkulApiGatewayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jamsoftwares/iskul_gateway/ISkulApiGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jamsoftwares.iskul_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISkulApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
